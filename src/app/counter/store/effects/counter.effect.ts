01546cea4ecd0d9f694122dcea564e0ddef9b9f5
omoshiteca <omoshiteca@gmail.com>
2019-09-11 11:03:10 +0900

    update

--- src/app/counter/store/effects/counter.effect.ts
+++ src/app/counter/store/effects/counter.effect.ts
@@ -3,13 +3,15 @@ import { Effect, Actions, ofType } from '@ngrx/effects';
 import { tap } from 'rxjs/operators';
 
 import { increment, decrement } from '../actions/counter.action';
+import { Observable } from 'rxjs';
+import { Action } from '@ngrx/store';
 
 @Injectable()
 export class CounterEffects {
   constructor(private actions$: Actions) {}
 
   @Effect({ dispatch: false })
-  increment$ = this.actions$.pipe(
+  increment$: Observable<Action> = this.actions$.pipe(
     ofType(increment.type),
     tap(action => {
       console.log(action);
